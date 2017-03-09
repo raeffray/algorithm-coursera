76fa75a65d506fd0ccd28de4143cff7a6d24619e
Renato Barbosa <renato.barbosa@sap.com>
2017-03-09 11:14:41 -0800

    polishing

--- percolation/src/main/java/algorithms/percolation/Percolation.java
+++ percolation/src/main/java/algorithms/percolation/Percolation.java
@@ -1,5 +1,8 @@
 package algorithms.percolation;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 import edu.princeton.cs.algs4.StdRandom;
 
 public class Percolation {
@@ -17,7 +20,16 @@ public class Percolation {
 	// the list of opened sites
 	private int[] openedSites;
 
+	// grid dimension
 	private int n;
+
+	// flag to export file results
+	private boolean exportResults;
+
+	// file to export the results
+	private String pathFileResults;
+
+	private PrintWriter out;
 
 	/**
 	 * create n-by-n grid, with all sites blocked
@@ -26,11 +38,14 @@ public class Percolation {
 	 *            matrix dimension (n x n)
 	 * 
 	 */
-	public Percolation(int n) {
+	public Percolation(final int n) {
 
 		this.n = n;
 
 		int gridSize = (int) Math.pow(n, 2);
+
+		// flag set to false by default;
+		exportResults = false;
 
 		virtualTopSite = gridSize;
 		virtualBottonSite = gridSize + 1;
@@ -50,10 +65,34 @@ public class Percolation {
 	}
 
 	/**
+	 * Additional constructor to be able to configure an export
+	 * file
+	 * 
+	 * @param n the grid dimension;
+	 * @param fileToExportResults file containing the results
+	 * 
+	 * */
+	public Percolation(final int n, final String fileToExportResults) {
+		this(n);
+		this.pathFileResults = fileToExportResults;
+		this.exportResults = true;
+	}
+
+	/**
 	 * open site (row, col) if it is not open already
+	 * 
+	 * @param row coordinate's row
+	 * @param col coordinate's column
+	 * "row index i out of bounds"
+	 * @throws IllegalArgumentException throws an {@link IllegalArgumentException} when
+	 * row or column are invalid 
 	 * 
 	 */
 	public void open(int row, int col) {
+
+		if (row < 1 || col < 1) {
+			throw new IllegalArgumentException();
+		}
 
 		int site = makePlainIndex(row, col);
 		int[] north = { row - 1, col };
@@ -66,9 +105,9 @@ public class Percolation {
 		int westIndex = makePlainIndex(west[0], west[1]);
 		int eastIndex = makePlainIndex(east[0], east[1]);
 
-		if (row == 0) {
+		if (row == 1) {
 			union(site, virtualTopSite);
-		} else if (row == n - 1) {
+		} else if (row == n) {
 			union(site, virtualBottonSite);
 		}
 
@@ -97,63 +136,102 @@ public class Percolation {
 	}
 
 	/**
-	 * is site (row, col) open?
-	 * 
+	 * Tells if a given coordinate is opened
 	 * O(1)
+	 * 
+	 * @param row coordinate's row
+	 * @param col coordinate's column
+	 * 
+	 * @return a boolean values
 	 * 
 	 */
 	public boolean isOpen(int row, int col) {
 		return openedSites[makePlainIndex(row, col)] == 1;
 	}
 
-	private int makePlainIndex(int row, int col) {
-		if (row < 0 || col < 0 || col > n-1 || row > n-1) {
+	/**
+	 * Transforms a coordinate into plain index
+	 * 
+	 * @param row coordinate's row
+	 * @param col coordinate's column
+	 * 
+	 * @return a integer number based on grid's dimension
+	 * 
+	 */
+	public int makePlainIndex(int row, int col) {
+		if (row < 1 || col < 1 || col > n || row > n) {
 			return -1;
 		}
 		if (row == 1) {
-			return n + col;
-		} else if (row == 0) {
-			return col;
+			return col - 1;
+		} else if (col == 1) {
+			return ((row - 1) * n);
+		} else if (col == n) {
+			return (row * n) - 1;
 		} else {
-			return (row * n) + col;
+			return ((row - 1) * n) + col - 1;
 		}
 	}
 
 	/**
-	 * is site (row, col) full?
+	 * Tells if a coordinate is connected to the virtual
+	 * roots, either top or bottom
+	 * 
+	 * @param row coordinate's row
+	 * @param col coordinate's column
+	 * 
+	 * @return boolean value
+	 * 
 	 */
 	public boolean isFull(int row, int col) {
 
-		return false;
+		int site = makePlainIndex(row, col);
+
+		int root = root(site);
+
+		return root == virtualBottonSite || root == virtualTopSite;
 	}
 
 	/**
-	 * number of open sites
+	 * retrieves the total of opened sites
+	 * @return the count
 	 * 
 	 */
 	public int numberOfOpenSites() {
-		return openedSites.length;
+		int count = 0;
+		for (int i = 0; i < openedSites.length; i++) {
+			int j = openedSites[i];
+			if (j == 1) {
+				count++;
+			}
+		}
+		return count;
 	}
 
 	/**
-	 * does the system percolate?
+	 * Tells if the system percolates
 	 * 
+	 *  @return a boolean value
 	 */
 	public boolean percolates() {
-		return grid[grid.length - 1] == virtualTopSite;
+		return grid[grid.length - 1] == grid[grid.length - 2];
 	}
 
-	// TODO: these methods are supposed to be private. they are opened for test
-	// purpose. Change it back to private afterwards.
-
-	public void union(final int from, final int to) {
+	private void union(final int from, final int to) {
 		int rootFrom = root(from);
 		int rootTo = root(to);
 
 		if (rootFrom == rootTo) {
 			return;
 		}
-		if (siteWeight[rootFrom] < siteWeight[rootTo]) {
+
+		if (rootTo == virtualBottonSite || rootTo == virtualBottonSite) {
+			grid[rootFrom] = rootTo;
+			siteWeight[rootTo] += siteWeight[rootFrom];
+		} else if (rootFrom == virtualBottonSite || rootFrom == virtualBottonSite) {
+			grid[rootTo] = rootTo;
+			siteWeight[rootFrom] += siteWeight[rootTo];
+		} else if (siteWeight[rootFrom] < siteWeight[rootTo]) {
 			grid[rootFrom] = rootTo;
 			siteWeight[rootTo] += siteWeight[rootFrom];
 		} else {
@@ -162,7 +240,7 @@ public class Percolation {
 		}
 	}
 
-	public int root(final int site) {
+	private int root(final int site) {
 		int root = site;
 		while (virtualTopSite != root && virtualBottonSite != root && root != grid[root]) {
 			root = grid[root];
@@ -170,12 +248,59 @@ public class Percolation {
 		return root;
 	}
 
-	public int getVirtualBottonSite() {
-		return virtualBottonSite;
+	/**
+	 * Run the percolation system, in a full closed grid
+	 * opening the grid with random numbers generated within
+	 * the grid's dimension borders.
+	 * 
+	 * */
+	public void run() {
+		int row = -1;
+		int col = -1;
+		openFile();
+		printValue(String.valueOf(n));
+		while (!percolates()) {
+			row = StdRandom.uniform(n + 1);
+			col = StdRandom.uniform(n + 1);
+			row = row == 0 ? 1 : row;
+			col = col == 0 ? 1 : col;
+			if (row > 0 && col > 0) {
+				if (!isOpen(row, col)) {
+					open(row, col);
+					printCoordinates(row, col);
+				}
+			}
+		}
+		closeFile();
 	}
 
-	public int getVirtualTopSite() {
-		return virtualTopSite;
+	private void printValue(final String value) {
+		if (exportResults) {
+			out.print(value);
+		}
 	}
 
+	private void printCoordinates(final int row, final int col) {
+		if (exportResults) {
+			printValue(row + " " + col + " ");
+		}
+	}
+
+	private void openFile() {
+		if (out == null && exportResults) {
+			try {
+				out = new PrintWriter(this.pathFileResults);
+			} catch (FileNotFoundException e) {
+				System.out.print("ERROR OPENING FILE. RESULTS WILL NOT BE EXPORTED");
+				exportResults = false;
+			}
+		}
+	}
+
+	private void closeFile() {
+		if (out != null) {
+			out.flush();
+			out.close();
+		}
+	}
 }
